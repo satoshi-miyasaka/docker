3ab1c12ae2b2abcf874dca383fe23dcc20e7f59b
satoshi-miyasaka <satoshi.miyasaka.1000@gmail.com>
2022-08-09 15:17:01 +0000

    ロジック完成 ソースは整理したい

--- python/test.py
+++ python/test.py
@@ -1,8 +1,17 @@
 #!/bin/python3
-# 最近出現していないボールを７つ選ぶ
-# ７個以下より少なくなってしまったら、最近よく出ている数字で残りを補完する
+# 全体から最近出現率の低いボールをｎ個
+# 最近出現率が低いボールをｍ個
+# 選ぶボール数がｌ個として
+# l - (n + m) = o で計算し、
+# 最近出現率の高いボールをｏ個選ぶ
+# ｎとｍのボールに重複がある場合、重複したボール数分ｏが増える
 
 import MySQLdb
+
+n = 3
+m = 3
+l = 7
+o = 0
 
 connection = MySQLdb.connect(
         host='loto',
@@ -13,10 +22,30 @@ connection = MySQLdb.connect(
 # cursor = connection.cursor(MySQLdb.cursors.DictCursor)
 cursor = connection.cursor()
 
+# 全体から出現率の少ないｎ個を選ぶ
+dict = {}
+for i in range(1, 44):
+    dict[i] = 0
+
+cursor.execute("SELECT ball_1, ball_2, ball_3, ball_4, ball_5, ball_6, bonus FROM loto6")
+rows = cursor.fetchall()
+
+for row in rows:
+    for col in row:
+        dict[col] += 1
+
+sort_dict = sorted(dict.items(), key=lambda x:x[1])
+
+marge_list = []
+for i in range(n):
+    marge_list.append(sort_dict[i][0])
+
 # 最初にリストに全ボールを入れて初期化
 list = []
+dict = {}
 for ball in range(1, 44):
     list.append(ball)
+    dict[ball] = 0
 
 # 新しい順に取得
 # cursor.execute("SELECT * FROM loto6 order by number desc")
@@ -24,44 +53,38 @@ cursor.execute("SELECT ball_1, ball_2, ball_3, ball_4, ball_5, ball_6, bonus FRO
 rows = cursor.fetchall()
 
 # リストから出現したボールを削除
+# 出現率の高いボールを選ぶため、辞書にボール数も補完する
+limit = 7
+flg = 1
 for row in rows:
-    for col in row:
-        if (0 < list.count(col)):
-            list.remove(col)
-    # 残ったボールが７個以下ならループを抜ける
-    # カラムの処理中に７個以下になっても抜けてはいけない
-    if (7 >= len(list)):
+    limit -= 1
+    if 0 >= limit and 0 == flg:
         break
 
-if (7 > len(list)):
-    list2 = []
-    cursor.execute("SELECT ball_1, ball_2, ball_3, ball_4, ball_5, ball_6, bonus FROM loto6 order by number desc")
-    rows = cursor.fetchall()
-    for row in rows:
-        list2.append(row[0])
-        list2.append(row[1])
-        list2.append(row[2])
-        list2.append(row[3])
-        list2.append(row[4])
-        list2.append(row[5])
-        list2.append(row[6])
-    
-        max = 0
-        for l in list2:
-            if max < list2.count(l):
-                max = list2.count(l)
+    for col in row:
+        dict[col] += 1
+        if (1 == flg and 0 < list.count(col)):
+            list.remove(col)
 
-        list3 = []
-        for l in list2:
-            if (max == list2.count(l)):
-                if (0 == list3.count(l)):
-                    list3.append(l) 
+    if (m >= len(list)):
+        flg = 0
 
-        if (7 == (len(list) + len(list3))):
-            list += list3
-            break
-list.sort()
-print(list)
+for i in list:
+    if (0 == marge_list.count(i)):
+        marge_list.append(i)
+
+# 最近出現率の高いボールを選ぶ
+sort_dict = sorted(dict.items(), reverse=True, key=lambda x:x[1])
+
+for o in sort_dict:
+    if 0 == marge_list.count(o[0]):
+        marge_list.append(o[0])
+
+    if l <= len(marge_list):
+        break
+
+marge_list.sort()
+print(marge_list)
 
 connection.commit()
 
