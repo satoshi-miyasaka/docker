9648d8aef2a84f1bd4dc9a34170ff85d110bbec2
satoshi-miyasaka <satoshi.miyasaka.1000@gmail.com>
2023-02-05 23:34:00 +0900

    第二版その２

--- python/PythonArrayTest.py
+++ python/PythonArrayTest.py
@@ -5,23 +5,24 @@ import sys
 
 def allAttackList(list, max):
     work = []
-    ret = []
 
+    # 最大数ならディープコピーしてリターン
     if max == len(list):
         for i in list: work.append(i)
-        ret.append(work)
-        return ret
+        return [work]
 
+    ret = []
+    # 配列をディープコピーしてから後ろから順に１文字削除
     for i in sorted(list, reverse=True):
         temp = []
         for j in list: temp.append(j)
         temp.remove(i)
+        # 再帰で呼び出し、二次元配列を返してもらう
         work += allAttackList(temp, max)
 
-    # 重複削除
+    # 重複している配列を削除する
     for l in work:
-        if 0 == ret.count(l):
-            ret.append(l)
+        if 0 == ret.count(l): ret.append(l)
 
     return ret
 
