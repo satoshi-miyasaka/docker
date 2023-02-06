e3485ab97f71a24a642153fddae56b6be9a72ebb
satoshi-miyasaka <satoshi.miyasaka.1000@gmail.com>
2023-02-06 22:12:36 +0900

    最終版か？

--- python/PythonArrayTest.py
+++ python/PythonArrayTest.py
@@ -4,48 +4,15 @@ import itertools
 
 # function area
 
-def allAttackList(list, max):
-    work = []
-
-    # 最大数ならディープコピーしてリターン
-    if max == len(list):
-        for i in list: work.append(i)
-        return [work]
-
-    ret = []
-    # 配列をディープコピーしてから後ろから順に１文字削除
-    for i in sorted(list, reverse=True):
-        temp = []
-        for j in list: temp.append(j)
-        temp.remove(i)
-        # 再帰で呼び出し、二次元配列を返してもらう
-        work += allAttackList(temp, max)
-
-    # 重複している配列を削除する
-    for l in work:
-        if 0 == ret.count(l): ret.append(l)
-
-    return ret
-
-def allAttackList2(argList, maxCount):
-
-    # 最大数ならディープコピーしてリターン
-    if maxCount == len(argList): return [[i for i in argList]]
-
+def makeBoxList(argList, maxCount):
     ret = []
     for a in itertools.product(argList, repeat = len(argList) - maxCount):
-        if len(a) == len(set(a)): continue
-
-        work = []
-        for i in argList:
-            if 0 == a.count(i): work.append(i)
-        ret.append(work)
-
+        if len(a) != len(set(a)) or list(a) != sorted(a): continue
+        ret.append([i for i in argList if 0 == a.count(i)])
     return sorted(ret)
 
 # main area
 
 if __name__ == '__main__':
     test = list(range(7))
-    print(allAttackList2(test, 7))
-
+    print(makeBoxList(test, 7))
